c29720b4e2885ca8467b8b97bfc6c248588be58e
federulli <fede.rulli@hotmail.com>
2016-08-27 15:22:56 -0300

    Agrego clase queue

--- src/main/java/ar/fiuba/tdd/template/Main.java
+++ src/main/java/ar/fiuba/tdd/template/Main.java
@@ -1,7 +1,25 @@
 package ar.fiuba.tdd.template;
 
+import ar.fiuba.tdd.template.queue.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is just a template project");
+        Queue<String> cola = new Queue<>();
+        if (cola.isEmpty()) {
+            System.out.println("TRUE");
+            cola.add("hola");
+            cola.add("pepe");
+            System.out.println(cola.top());
+            cola.remove();
+            System.out.println(cola.top());
+            cola.remove();
+            System.out.println(cola.isEmpty());
+
+        } else {
+            System.out.println(cola.size());
+            System.out.println("FFALSE");
+        }
+
     }
 }
