acfaf32e36d8083e728e41bfc20f5bc0a599f55b
federulli <fede.rulli@hotmail.com>
2016-08-28 13:50:46 -0300

    limpio el main

--- src/main/java/ar/fiuba/tdd/template/Main.java
+++ src/main/java/ar/fiuba/tdd/template/Main.java
@@ -1,25 +1,8 @@
 package ar.fiuba.tdd.template;
 
-import ar.fiuba.tdd.template.queue.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Queue<String> cola = new Queue<>();
-        if (cola.isEmpty()) {
-            System.out.println("TRUE");
-            cola.add("hola");
-            cola.add("pepe");
-            System.out.println(cola.top());
-            cola.remove();
-            System.out.println(cola.top());
-            cola.remove();
-            System.out.println(cola.isEmpty());
-
-        } else {
-            System.out.println(cola.size());
-            System.out.println("FFALSE");
-        }
-
+        System.out.println("hola");
     }
 }
